bbb1a3a92c9955ad678952484a24e9c3dd2c1109
ippsav <mehdiboujid@gmail.com>
2022-01-15 17:23:30 +0100

    test for Average number function

--- main_test.go
+++ main_test.go
@@ -32,3 +32,29 @@ func TestTestValidity(t *testing.T) {
 		})
 	}
 }
+
+func TestAverageNumber(t *testing.T) {
+	tt := []struct {
+		name          string
+		input         string
+		averageNumber float32
+	}{
+		{
+			name:          "Should pass",
+			input:         "23-ab-48-cab-41-hahaa",
+			averageNumber: 37.333332,
+		},
+		{
+			name:          "Should not pass",
+			input:         "23-41-hahaa",
+			averageNumber: -1,
+		},
+	}
+	for _, tc := range tt {
+		t.Run(tc.name, func(tr *testing.T) {
+			if out, _ := AverageNumber(tc.input); out != tc.averageNumber {
+				tr.Errorf("validate failed. Expected %v, got: %v", tc.averageNumber, out)
+			}
+		})
+	}
+}

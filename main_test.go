19423bf7aa917fe89ee5580af6965de0b6d4c14a
ippsav <mehdiboujid@gmail.com>
2022-01-18 21:49:47 +0000

    test for testValidity with regex

--- main_test.go
+++ main_test.go
@@ -36,6 +36,36 @@ func TestTestValidity(t *testing.T) {
 	}
 }
 
+func TestTestValidityWithRegex(t *testing.T) {
+	tt := []struct {
+		name  string
+		input string
+		ok    bool
+	}{
+		{
+			name:  "Should pass",
+			input: "23-ab-48-cab-41-hahaa",
+			ok:    true,
+		},
+		{
+			name:  "Should not pass",
+			input: "23-ab-48-41-hahaa",
+			ok:    false,
+		},
+		{
+			name:  "Should not pass",
+			input: "random string",
+			ok:    false,
+		},
+	}
+	for _, tc := range tt {
+		t.Run(tc.name, func(tr *testing.T) {
+			if out := testValidityWithRegex(tc.input); out != tc.ok {
+				tr.Errorf("validate failed. Expected %v, got: %v", tc.ok, out)
+			}
+		})
+	}
+}
 func TestAverageNumber(t *testing.T) {
 	tt := []struct {
 		name          string
